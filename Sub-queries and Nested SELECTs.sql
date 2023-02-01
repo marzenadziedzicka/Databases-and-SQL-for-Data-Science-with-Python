476b2df5b56b79fb553916e501b1c6bb3cfd6b16
Marzena Dziedzicka <111153054+marzenadziedzicka@users.noreply.github.com>
2023-02-01 12:33:23 +0100

    selec - from - where - select, max, avg

--- Sub-queries and Nested SELECTs.sql
+++ Sub-queries and Nested SELECTs.sql
@@ -12,5 +12,5 @@ select EMP_ID, SALARY, MAX(SALARY) as MAX_SALARY from EMPLOYEES;
 select EMP_ID, SALARY, (select MAX(SALARY) from EMPLOYEES as MAX_SALARY) from EMPLOYEES;
 
 --Execute a Table Expression for the EMPLOYEES table that excludes columns with sensitive employee data (i.e. does not include columns: SSN, B_DATE, SEX, ADDRESS, SALARY).
-select * from ( select EMP_ID, F_NAME, L_NAME, DEP_ID from employees) AS EMP4ALL;
+select * from ( select EMP_ID, F_NAME, L_NAME, DEP_ID from employees) AS EMP4ALL; 
 
